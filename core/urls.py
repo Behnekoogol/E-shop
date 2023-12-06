2eb983380e5b8ca6a104eb9ed4403c0cc070aa4d
“Behnekoogol” <behnekoogol@gmail.com>
2023-12-06 13:03:43 +0330

    testing product model

--- core/urls.py
+++ core/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls'))
 ]
 
 
--- shop/models.py
+++ shop/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
     slug = models.SlugField(verbose_name=_('slug'), allow_unicode = True, null=False)
     description = models.TextField(verbose_name=_("description"), null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
-    gender = models.ForeignKey(Gender, on_delete=models.CASCADE, verbose_name=_('gender'), null=True)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE, verbose_name=_('gender'), null=True, blank=True)
     price = models.PositiveIntegerField(verbose_name=_("price"))
     in_stock = models.BooleanField(verbose_name=_("in stock"), default=True)
     brand = models.CharField(max_length=100, null=True, blank=True)
