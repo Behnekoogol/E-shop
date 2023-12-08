e6fbaa1b054e4ef6730c7bc0d304c338cf63f57f
abbaskaf <hashemkafeshani@gmail.com>
2023-12-08 17:47:27 +0330

    template & css & view & url

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,10 @@ from .import views
 
 
 urlpatterns = [
-    path('', views.product_list)
+    path('', views.product_list),
+    path('product.html', views.product),
+    path('<int:pk>', views.Single.as_view(), name='single'),
+    path('about.html', views.About),
+    path('contact.html', views.Contact),
+
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
-from .models import Product 
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product ,Category
+from django.views.generic import TemplateView, DetailView, ListView
 
 
 def product_list(request):
+    category = Category.objects.all()
     products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
+    return render(request, 'index.html', {'products': products,'category':category})
 
+
+class About:
+    pass
+
+
+class Contact:
+    pass
+
+
+class Single(DetailView):
+    template_name = 'single-product.html'
+    model = Product
+
+
+def product(request):
+    return None
