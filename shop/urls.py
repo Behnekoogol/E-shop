4f64fee9a5a13d26bcdd6d4f5b83e8582f142f61
abbaskaf <hashemkafeshani@gmail.com>
2023-12-10 21:45:24 +0330

    template &  view & url

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,8 @@ from .import views
 
 
 urlpatterns = [
-    path('', views.product_list),
-    path('product.html', views.product),
+    path('', views.product_list,name='home'),
+    path('category/<int:category_id>/', views.Products.as_view(),name='products'),
     path('<int:pk>', views.Single.as_view(), name='single'),
     path('about.html', views.About),
     path('contact.html', views.Contact),
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product ,Category
+from .models import Product, Category
 from django.views.generic import TemplateView, DetailView, ListView
 
 
 def product_list(request):
     category = Category.objects.all()
     products = Product.objects.all()
-    return render(request, 'index.html', {'products': products,'category':category})
+    return render(request, 'index.html', {'products': products, 'category': category})
 
 
 class About:
@@ -22,5 +22,16 @@ class Single(DetailView):
     model = Product
 
 
-def product(request):
-    return None
+class Products(TemplateView):
+    template_name = 'products.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = kwargs['category_id']
+        post = get_object_or_404(Category, id=category_id)
+        posty = Product.objects.filter(category=post)
+        cater = Category.objects.all()
+        context['post'] = post
+        context['posty'] = posty
+        context['cater'] = cater
+        return context
